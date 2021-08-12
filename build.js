ed4caa2bea9cba2dcb5669d82b8e749d247227ab
Youssef Hanna <youssef.hanna@me.com>
2021-08-12 10:46:38 +0200

    Update build.js

---
+++ build.js
@@ -0,0 +1,25 @@
+{
+    "response": [
+        {
+            "datetime": "1626609025",
+            "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-0735.zip",
+            "id": "00161cc91ed70f1ba116bb31b049784f",
+            "romtype": "UNOFFICIAL",
+            "size": "716843146",
+            "url": "https://downloads.droidontime.com/dot11/oneplus3/vanilla/dotOS-R-v5.1.2-oneplus3-OFFICIAL-20210718-1116.zip",
+            "version": "v5.1.2",
+            "changelog": [
+                {
+                    "miscTitle": "New features",
+                    "miscSummary": "\u2022 Added colored statusbar icons toggle \n\u2022 Added Programming IDE lockscreen clock \n\u2022 Added Samsung Lockscreen clock \n\u2022 Added dual tone clock.",
+                    "settingsTitle": "Small layout fixes",
+                    "settingsSummary": "\u2022 Face Unlock improvements.",
+                    "securityTitle": "July Bulletin",
+                    "securitySummary": "\u2022 July Security Patch merged",
+                    "systemTitle": "Locksceen clock fixes",
+                    "systemSummary": "\u2022 Fixes for Lockscreen clock when qspanel is expanded\n\u2022 fixed Theme reset after reboot\n\u2022 privacy indicators are back\n\u2022 Merged translations"
+                }
+            ]
+        }
+    ]
+}
