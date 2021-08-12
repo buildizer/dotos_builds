7b91b8aeba1d47c81bb1330a2e8c67b8ef9dd3f9
Youssef Hanna <youssef.hanna@me.com>
2021-08-12 18:35:51 +0200

    Update wifi fix

--- dot_x3/build.js
+++ dot_x3/build.js
@@ -1,19 +1,25 @@
 {
-     "response": [
-         {
-             "datetime": "1628792604",
-             "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip",
-             "id": "c2753f51bfe1350268e7f133be4f195f",
-             "romtype": "UNOFFICIAL",
-             "size": "1288490188",
-             "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip/download",
-             "version": "v5.1.3",
-             "changelog": [
-                 {
-                     "miscTitle": "Hot Fix",
-                     "miscSummary": "\u2022 Fix Wifi on Realme X3 Devices."
-                 }
-             ]
-         }
-     ]
- }
+    "response": [
+        {
+            "datetime": "1628778204",
+            "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip",
+            "id": "c2753f51bfe1350268e7f133be4f195e",
+            "romtype": "UNOFFICIAL",
+            "size": "1288490188",
+            "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip/download",
+            "version": "v5.1.3",
+            "changelog": [
+                {
+                    "miscTitle": "Hot Fix",
+                    "miscSummary": "\u2022 Fix Wifi on Realme X3 Devices.",
+                    "settingsTitle": "Small layout fixes",
+                    "settingsSummary": "\u2022 Face Unlock improvements.",
+                    "securityTitle": "August Bulletin",
+                    "securitySummary": "\u2022 August Security Patch merged",
+                    "systemTitle": "Locksceen clock fixes",
+                   "systemSummary": "\u2022 Fixes for Lockscreen clock when qspanel is expanded"
+                }
+            ]
+        }
+    ]
+}
