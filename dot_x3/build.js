dd791123556cdc62dd0f2770834dfb8be730d304
Youssef Hanna <youssef.hanna@me.com>
2021-08-12 18:25:23 +0200

    Update build.js

--- dot_x3/build.js
+++ dot_x3/build.js
@@ -4,7 +4,7 @@
              "datetime": "1628778204",
              "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip",
              "id": "c2753f51bfe1350268e7f133be4f195e",
-             "romtype": "OFOFFICIAL",
+             "romtype": "UNOFFICIAL",
              "size": "1288490188",
              "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip/download",
              "version": "v5.1.3.1",
