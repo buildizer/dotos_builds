d436f827ebb03b9c72eff1872abae278f5a45191
Youssef Hanna <youssef.hanna@me.com>
2021-08-12 18:24:48 +0200

    Update build.js

--- dot_x3/build.js
+++ dot_x3/build.js
@@ -1,1 +1,19 @@
-
+{
+     "response": [
+         {
+             "datetime": "1628778204",
+             "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip",
+             "id": "c2753f51bfe1350268e7f133be4f195e",
+             "romtype": "OFOFFICIAL",
+             "size": "1288490188",
+             "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip/download",
+             "version": "v5.1.3.1",
+             "changelog": [
+                 {
+                     "miscTitle": "Hot Fix",
+                     "miscSummary": "\u2022 Fix Wifi on Realme X3 Devices.",
+                 }
+             ]
+         }
+     ]
+ }
