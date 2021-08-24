d3819c2ac9987432e89632081ec85d4e9799474b
Youssef Hanna <youssef.hanna@me.com>
2021-08-24 23:39:36 +0200

    Update builds_gapps.js

--- dot_x3/builds_gapps.js
+++ dot_x3/builds_gapps.js
@@ -1,17 +1,17 @@
 {
      "response": [
          {
-             "datetime": "1628786360",
-             "filename": "dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip",
-             "id": "c2753f51bfe1350268e7f133be4f195e",
+             "datetime": "1629840368",
+             "filename": "dotOS-R-v5.1.3.1-x3-GAPPS-20210824-2126.zip",
+             "id": "143ee5914a4e07a3514f0c7bc12aef83",
              "romtype": "UNOFFICIAL",
-             "size": "1288490188",
-             "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3-x3-UNOFFICIAL-20210812-1531.zip/download",
-             "version": "v5.1.3",
+             "size": "1081650728",
+             "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.1.3.1-x3-GAPPS-20210824-2126.zip/download",
+             "version": "v5.1.3.1",
              "changelog": [
                  {
-                     "miscTitle": "Hot Fix",
-                     "miscSummary": "\u2022 Fix Wifi on Realme X3 Devices."
+                     "miscTitle": "Performance",
+                     "miscSummary": "\u2022 Fix some issues in touch and gapps."
                  }
              ]
          }
