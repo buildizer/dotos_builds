a84e87a3b2aeffbb1c482d31174fe400e3931198
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 01:45:28 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -3,7 +3,7 @@
          {
              "datetime": "1631749182",
              "filename": "dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip",
-             "id": "20bcf247615056e6ea227aa49a95696e",
+             "id": "9672499ad1aea9a655ff220d221bc89e",
              "romtype": "GAPPS",
              "size": "1098297300",
              "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip/download",
