c42dbf2f3798f4ed483b1f5531793632a85d1083
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:38:43 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -1,21 +1,3 @@
-#{
-#  codename: "dot_x3",
-#  deviceName: "RealmeX3 / RealmeX3 SupeZoom",
-#  brandName: "Realme",
-#  releases: [
-#    {
-#      type: "gapps",
-#      generatedAt: 1631796700,
-#      fileName: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
-#      url: "http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
-#     requireCleanFlash: false,
-#      images: null,
-#      hash: "43ed60c118d9c8e0582679d699b27d1f",
-#      size: 1118127940,
-#      version: "v5.2.0.1"
-#    }
-#  ]
-#}
 {
   codename: "dot_x3",
   deviceName: "RealmeX3 / RealmeX3 SupeZoom",
@@ -23,7 +5,7 @@
   releases:[
     {
       generatedAt: 1631796700,
-      filename: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
+      fileName: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       hash: "43ed60c118d9c8e0582679d699b27d1f",
       romtype: "gapps",
       requireCleanFlash: false,
