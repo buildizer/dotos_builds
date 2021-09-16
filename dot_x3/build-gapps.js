5aa2af800e6d78a6910868ddbcc37e6dbea17948
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:39:00 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -11,7 +11,7 @@
       requireCleanFlash: false,
       size: 1118127940,
       images: null,
-      "url": "http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
+      url: "http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       version:"v5.2.0.1"
     }
   ]
