af5ec05fe60aff4fceb6ea30a61dfc5311ea216d
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 02:04:26 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -1,25 +1,18 @@
 {
-     "response": [
-         {
-             "datetime": "1631749182",
-             "filename": "dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip",
-             "id": "9672499ad1aea9a655ff220d221bc89e",
-             "romtype": "GAPPS",
-             "size": "1118199056",
-             "url": "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip/download",
-             "version": "v5.2.1",
-             "changelog": [
-                 {
-                     "miscTitle": "",
-                     "miscSummary": "Fix system crashes \n\u2022 Add more space in partion size to install gapps",
-                     "settingsTitle": "",
-                     "settingsSummary": "",
-                     "securityTitle": "September Bulletin",
-                     "securitySummary": "\u2022 September Security Patches (Android 11 r45)",
-                     "systemTitle": "",
-                     "systemSummary": "What's new on System and Customizations: \n\n\u2022 September Security Patches (Android 11 r45) \n\u2022 MonetWannabe updated to 2.2 \n\u2022 Rebased theme engine to latest kdrag0n's ZCAM implementation (Way better color generation) \n\u2022 Added Wallpaper Color picker support \n\u2022 Added Theme Colorfulness control \n\u2022 Added Theme Brightness control \n\u2022 Added independent Palette of colors for both homescreen and lockscreen wallpapers \n\u2022 Added support for global Monet support (Customizations, Settings, SystemUI, core elements etc) \n\u2022 Moved color generation to SystemUI thread \n\n Revamped Customizations app \n\n\u2022 Almost completely redesigned to ensure a better UX \n\u2022 Implemented Wallpaper Scheduling \n\u2022 Added Monet support (some apps which supports Android12 themeing) \n\n Gaming Mode \n\n\u2022 Added 'Game Dashboard' \n\u2022 Implemented Gaming Mode but with a better UI and UX \n\u2022 Implemented an early version of 'Quick Control' a floating pill with handy controls while you're gaming \n\n Battery Manager \n\n\u2022 Initially based on Buoy - a battery saver profile customization app \n\u2022 Moved here everything that's battery related \n\u2022 Added Battery Saver Profiles \n\u2022 Added Battery Saver Profile customization \n\u2022 Added Smart Charge \n\u2022 Added Smart Cut-off \n\n Misc \n\n\u2022 updated translations from crowdin \n\u2022 UI/UX improvements \n\u2022 Stability and underhood improvements"
-                 }
-             ]
-         }
-     ]
- }
+  codename: "x3",
+  deviceName: "RealmeX3",
+  brandName: "Realme",
+  releases: [
+    {
+      type: "gapps",
+      generatedAt: 1631749182,
+      fileName: "dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip",
+      url: "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.2.1-x3-GAPPS-20210915-2306.zip/download",
+      requireCleanFlash: false,
+      images: null,
+      hash: "66f0e8bc8d9b64f90ff995de7b5ebc7b",
+      size: 1118199056,
+      version: "v5.2.1"
+    }
+  ]
+}
