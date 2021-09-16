0b4c46d4b77467419525942bf8b45007f11642b8
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:19:36 +0200

    Update and rename build-gapps.js1 to build-gapps.js

---
+++ dot_x3/build-gapps.js
@@ -0,0 +1,18 @@
+{
+  codename: "dot_x3",
+  deviceName: "RealmeX3 / RealmeX3 SupeZoom",
+  brandName: "Realme",
+  releases: [
+    {
+      type: "gapps",
+      generatedAt: 1631796700,
+      fileName: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
+      url: "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip/download",
+      requireCleanFlash: false,
+      images: null,
+      hash: "43ed60c118d9c8e0582679d699b27d1f",
+      size: 1118127940,
+      version: "v5.2.0.1"
+    }
+  ]
+}
