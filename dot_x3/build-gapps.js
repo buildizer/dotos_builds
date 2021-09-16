cbc58a76b9cb16ace59e376d5557c2d0feb36cb6
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:35:50 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -22,10 +22,10 @@
   brandName: "Realme",
   "response":[
     {
-      "datetime":1631796700,
-      "filename":"dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
-      "id":"43ed60c118d9c8e0582679d699b27d1f",
-      "romtype":"GAPPS",
+      generatedAt:1631796700,
+      filename:"dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
+      hash:"43ed60c118d9c8e0582679d699b27d1f",
+      romtype:"GAPPS",
       "size":1118127940,
       "url":"http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       "version":"v5.2.0.1",
