9869ebc0468e0e9ba924d91c5529e7a9dfa9412a
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:31:22 +0200

    Update and rename build-gapps.js1 to build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -7,7 +7,7 @@
       type: "gapps",
       generatedAt: 1631796700,
       fileName: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
-      url: "https://sourceforge.net/projects/dotosx3/files/dot11/dot_x3/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip/download",
+      url: "http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       requireCleanFlash: false,
       images: null,
       hash: "43ed60c118d9c8e0582679d699b27d1f",
