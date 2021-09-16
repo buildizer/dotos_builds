e4794b0840dc2c1ffd3b7c238537fbf8cfacbea9
Youssef Hanna <youssef.hanna@me.com>
2021-09-16 15:37:43 +0200

    Update build-gapps.js

--- dot_x3/build-gapps.js
+++ dot_x3/build-gapps.js
@@ -26,7 +26,9 @@
       filename: "dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       hash: "43ed60c118d9c8e0582679d699b27d1f",
       romtype: "GAPPS",
+      requireCleanFlash: false,
       size: 1118127940,
+      images: null,
       "url": "http://164.90.193.44:8080/dotOS-R-v5.2.0.1-x3-GAPPS-20210916-1251.zip",
       version:"v5.2.0.1"
     }
